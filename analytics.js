b1ff2a01eddab91c5b7eb6f87f99ac30ba2500a1
Mateus Miguel <webmateusmiguel@gmail.com>
2018-04-27 14:44:00 -0300

    test

--- analytics.js
+++ analytics.js
@@ -1,18 +1,4 @@
-
-$('#page').on('click', function () {
-    gtag('event', 'enviar-tarefa', {
-        'method': 'quiz',
-    });
-});
-
-// gtag('event', 'click'>, {
-//     'event_category': 'home',
-//     'event_label': 'btn-home',
-//     'value': <value>
-//   });
-
-
-  function trackClick(site, page) {
+function trackClick(site, page) {
     gtag('event', 'click', {
         'event_category': site,
         'event_label': page
